033d798df125aae568eb5de7d79f28881f10659b
Willians Freitas <williansvpf@gmail.com>
2025-08-16 18:38:49 -0300

    gerado migration das tabelas do identity

--- Migrations/20250813200234_CriarTabelasIdentity.Designer.cs
+++ Migrations/20250813200234_CriarTabelasIdentity.Designer.cs
@@ -4,6 +4,7 @@ using ASP.NET.Core.Identity.Models.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASP.NET.Core.Identity.Migrations
 {
     [DbContext(typeof(SisMedContext))]
-    partial class SisMedContextModelSnapshot : ModelSnapshot
+    [Migration("20250813200234_CriarTabelasIdentity")]
+    partial class CriarTabelasIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
